9f02f1b1a0e4e89910b245349423d9fe718206de
NahumMejia <nahummejiad@gmail.com>
2025-03-11 10:22:21 -0600

    Installed filament and Admin Panel for each role

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,9 +4,8 @@ namespace Database\Seeders;
 
 use App\Enums\PermissionsEnum;
 use App\Enums\RolesEnum;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission as ModelsPermission;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
@@ -16,28 +15,31 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //Create roles
+        // Create roles
         $userRole = Role::create(['name' => RolesEnum::User->value]);
         $vendorRole = Role::create(['name' => RolesEnum::Vendor->value]);
         $adminRole = Role::create(['name' => RolesEnum::Admin->value]);
-        //Create permissions
-        $approveVendors = ModelsPermission::create(['name' =>PermissionsEnum::ApproveVendors->value]);
-        $sellProducts = ModelsPermission::create(['name' =>PermissionsEnum::SellProducts->value]);
-        $buyProducts = ModelsPermission::create(['name' =>PermissionsEnum::BuyProducts->value]);
-        //Assign permissions to roles
-        $userRole->givePermissionTo([
-            $buyProducts,
+
+        // Create permissions
+        $approveVendors = Permission::create([
+            'name' => PermissionsEnum::ApproveVendors->value
+        ]);
+        $sellProducts = Permission::create([
+            'name' => PermissionsEnum::SellProducts->value
+        ]);
+        $buyProducts = Permission::create([
+            'name' => PermissionsEnum::BuyProducts->value
         ]);
 
-        $vendorRole->givePermissionTo([
-            $buyProducts, 
-            $sellProducts,
+        // Assign permissions to roles
+        $userRole->syncPermissions([
+            $buyProducts
         ]);
-
-        $adminRole->givePermissionTo([
-            $buyProducts, 
-            $sellProducts, 
-            $approveVendors,
+        $vendorRole->syncPermissions([
+            $buyProducts, $sellProducts
+        ]);
+        $adminRole->syncPermissions([
+            $buyProducts, $sellProducts, $approveVendors
         ]);
     }
 }
--- resources/js/Pages/Auth/Register.tsx
+++ resources/js/Pages/Auth/Register.tsx
@@ -3,7 +3,6 @@ import InputLabel from '@/Components/Core/InputLabel';
 import PrimaryButton from '@/Components/Core/PrimaryButton';
 import TextInput from '@/Components/Core/TextInput';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import GuestLayout from '@/Layouts/GuestLayout';
 import { Head, Link, useForm } from '@inertiajs/react';
 import { FormEventHandler } from 'react';
 
@@ -28,7 +27,7 @@ export default function Register() {
             <Head title="Register" />
 
             <div className={'p-8'}>
-                <div className="card bg-white shadow max-w-[420px] mx-auto dark: bg-gray-800">
+                <div className="card bg-white shadow max-w-[420px] mx-auto dark:bg-gray-800">
                     <div className="card-body">
                     <form onSubmit={submit}>
                 <div>
