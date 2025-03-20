4bc000b196788a6dbd47e0a51287b7d65acdbaca
NahumMejia <nahummejiad@gmail.com>
2025-03-19 21:18:55 -0600

    Implement Product images

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
+    use InteractsWithMedia;
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')->width(100);
+        $this->addMediaConversion('small')->width(480);
+        $this->addMediaConversion('large')->width(1200);
+    }
+
     public function department(){
         return $this->belongsTo(Department::class);
     }
